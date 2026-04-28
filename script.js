abef89cdde8a4d998d22996d7162cb16c5e8ee1e
SUDOM4GE <133503215+SUDOM4GE@users.noreply.github.com>
2026-04-28 22:38:38 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function randomText() {
-  const texts = ["meow ", "meOW", "meowwww:3", "meow meow"];
+  const texts = ["meow ", "meOW", "meowwww:3", "meow meow", "MEOW", "meoooow"];
   const random = texts[Math.floor(Math.random() * texts.length)];
   document.getElementById("meow").innerText = random;
 }
